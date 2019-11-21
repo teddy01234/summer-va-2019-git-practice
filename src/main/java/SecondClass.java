2062187974846a308a795fb150489e5414f37b9c
teddy01234 <mee703@hotmail.com>
2019-11-21 16:49:10 -0500

    another commit

--- src/main/java/SecondClass.java
+++ src/main/java/SecondClass.java
@@ -2,6 +2,7 @@ public class SecondClass {
     public static void main(String[] args) {
         String myname = "Hsads";
         System.out.println(myname);
+        System.out.println("this is another lie");
 
     }
 }
