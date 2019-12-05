e94bd470b7ea21dee0906bc2cd09526c15ecc7f9
teddy <55107100+teddy01234@users.noreply.github.com>
2019-12-05 15:29:45 -0500

    Update FirstClass.java

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -1,6 +1,6 @@
 public class FirstClass {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world all");
         System.out.println("Hello cybertek");
         System.out.println("i am working until 5.10");
 
