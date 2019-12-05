6dc5b92d7f43fecce506f7aecd84896d205ff37a
teddy01234 <mee703@hotmail.com>
2019-12-05 15:43:18 -0500

    sadada

--- src/main/java/FirstClass.java
+++ src/main/java/FirstClass.java
@@ -2,7 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hello world all");
         System.out.println("Hello cybertek");
-        System.out.println("i am working until 5.10");
+        System.out.println("i am working until 5.40");
 
     }
 }
