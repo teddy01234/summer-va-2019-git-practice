648ee1e33578dbde4de58de5be264f2aaa302966
teddy <55107100+teddy01234@users.noreply.github.com>
2019-12-05 15:45:31 -0500

    Update SecondClass.java

--- src/main/java/SecondClass.java
+++ src/main/java/SecondClass.java
@@ -1,6 +1,6 @@
 public class SecondClass {
     public static void main(String[] args) {
-        String myname = "Hsads";
+        String myname = "Luiss";
         System.out.println(myname);
         System.out.println("this is another line");
         System.out.println("this is another line");
